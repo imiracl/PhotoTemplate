96c9c1c7da62a75e590962692556166c28c06573
imiracl <38769648+imiracl@users.noreply.github.com>
2021-01-28 22:34:27 +0300

    starting slider section

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,50 @@
-$(function() {
+$(function () {
 
 	// Custom JS
-
+	$('.vertical-center-4').slick({
+		dots: true,
+		infinite: false,
+		speed: 300,
+		slidesToShow: 2,
+		slidesToScroll: 2,
+		rows: 2,
+		//fade: true,
+		//asNavFor: '.slider-nav',
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+					infinite: true,
+					dots: true
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+			// You can unslick at a given breakpoint now by adding:
+			// settings: "unslick"
+			// instead of a settings object
+		]
+	});
+	$('.slider-nav').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		asNavFor: '.slider-for',
+		dots: true,
+		centerMode: true,
+		focusOnSelect: true
+	});
 });
